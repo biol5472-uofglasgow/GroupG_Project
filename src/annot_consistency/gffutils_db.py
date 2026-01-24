8bc31fbeb8280a373d3ebd2f4be18add1b1cf1d4
unknown <sahesh02@gmail.com>
2026-01-24 16:40:14 +0000

    Added type checking for the load_or_create_db function

--- src/annot_consistency/gffutils_db.py
+++ src/annot_consistency/gffutils_db.py
@@ -1,11 +1,14 @@
 import gffutils
 import os
+from typing import Tuple
+from pathlib import Path
+from gffutils.interface import FeatureDB
 
 
 #Function to load the pre-existing database or create one for gff files 
 # If DB exists, it connects to it
 # If DB does not exist, it creates a DB using the gff file
-def load_or_create_db(gff_file_a, gff_file_b,  db_path_a, db_path_b):
+def load_or_create_db(gff_file_a: Path, gff_file_b: Path, db_path_a: Path, db_path_b: Path) -> Tuple[FeatureDB, FeatureDB]:
 
     if os.path.isfile(db_path_a):
         db_a = gffutils.FeatureDB(db_path_a)
@@ -21,4 +24,5 @@ def load_or_create_db(gff_file_a, gff_file_b,  db_path_a, db_path_b):
        db_b = gffutils.create_db(gff_file_b, dbfn = db_path_b, keep_order = True)
 
     return db_a, db_b
+
 
