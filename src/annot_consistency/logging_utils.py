09abdbc233752fa923839da906727b421b67bf57
KaiserWaffles <148008162+KaiserWaffles@users.noreply.github.com>
2026-01-24 13:47:32 +0000

    Update logging_utils to avoid duplicate logs on reruns

--- src/annot_consistency/logging_utils.py
+++ src/annot_consistency/logging_utils.py
@@ -1,12 +1,15 @@
 import logging
 
-def logger(log_file: str) -> logging.logger:
+def logger(log_file: str) -> logging.Logger:
     logger = logging.getLogger("gffACAKE")
-    logger.setLevel(logging.DEBUG)                                              # Set level to Debug
-    logger.handlers.clear()                                                     # prevent duplicate logs if rerun
+    logger.setLevel(logging.DEBUG)      # Set level to Debug; capture everything
+    if logger.handlers:                                           
+        logger.handlers.clear()     # prevent duplicate logs if rerun
     format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")       # logger output message
     filehandler = logging.FileHandler(log_file)
-    filehandler.setLevel(logging.Debug)
+    filehandler.setLevel(logging.DEBUG)
     filehandler.setFormatter(format)
     logger.addHandler(format)
+    logger.propagate = False        # Prevent log records from propagating to parent loggers.
     return logger
+
