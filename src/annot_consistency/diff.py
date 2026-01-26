fad85c0ac679873f73107e4d97c19d5569cb3245
kro7ks <kksameer98@gmail.com>
2026-01-26 16:40:25 +0000

    Wrote a function to provide the details that are changed in the signatures between release A and release B to be used in changes.tsv

---
+++ src/annot_consistency/diff.py
@@ -0,0 +1,26 @@
+from annot_consistency.models import EntitySummary
+from typing import List
+
+# Writing function for checking through each attribute in the signature if they are different
+def changed_details(a: EntitySummary, b: EntitySummary) -> str:
+    parts: List[str] = []
+    if a.seqid != b.seqid:
+        parts.append(f'seqid: {a.seqid} -> {b.seqid}')
+    if a.source != b.source:
+        parts.append(f'Source: {a.source} -> {b.source}')
+    if a.entity_type != b.entity_type:
+        parts.append(f'Entity Type: {a.entity_type} -> {b.entity_type}')
+    if a.start != b.start:
+        parts.append(f'Start: {a.start} -> {b.start}')
+    if a.end != b.end:
+        parts.append(f'End: {a.end} -> {b.end}')
+    if a.strand != b.strand:
+        parts.append(f'Strand: {a.strand} -> {b.strand}')
+    if a.parent_id != b.parent_id:
+        parts.append(f'Parent ID: {a.parent_id} -> {b.parent_id}')
+    if a.phase != b.phase:
+        parts.append(f'Phase: {a.phase} -> {b.phase}')
+    if a.score != b.score:
+        parts.append(f'Score: {a.score} -> {b.score}')
+    
+    return parts
