132545fd324057e617f016ac761724114fe2ff2b
unknown <sahesh02@gmail.com>
2026-01-27 23:31:42 +0000

    removing checking if file exists using os in load_or_create_db

--- src/annot_consistency/gffutils_db.py
+++ src/annot_consistency/gffutils_db.py
@@ -1,28 +1,27 @@
 import gffutils
-import os
-from typing import Tuple
+from typing import tuple
 from pathlib import Path
 from gffutils.interface import FeatureDB
-
 
 #Function to load the pre-existing database or create one for gff files 
 # If DB exists, it connects to it
 # If DB does not exist, it creates a DB using the gff file
-def load_or_create_db(gff_file_a: Path, gff_file_b: Path, db_path_a: Path, db_path_b: Path) -> Tuple[FeatureDB, FeatureDB]:
 
-    if os.path.isfile(db_path_a):
+def load_or_create_db(gff_file_a: Path, gff_file_b: Path, db_path_a: Path, db_path_b: Path) -> tuple[FeatureDB, FeatureDB]:
+
+    try: 
         db_a = gffutils.FeatureDB(db_path_a)
-        
-    else:
-        db_a = gffutils.create_db(gff_file_a, dbfn = db_path_a, keep_order = True)           
+
+    except:
+        db_a = gffutils.create_db(gff_file_a, dbfn = db_path_a, keep_order = True)        
+
             
-    
-    if os.path.isfile(db_path_b):
+    try:
         db_b = gffutils.FeatureDB(db_path_b)
             
-    else:
+    except:
        db_b = gffutils.create_db(gff_file_b, dbfn = db_path_b, keep_order = True)
 
-    return db_a, db_b
 
+    return db_a, db_b
 
