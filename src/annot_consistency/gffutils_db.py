30025e7a1ec20baed327ba4a43d600af0371a645
unknown <sahesh02@gmail.com>
2026-01-28 00:44:25 +0000

    converting paths to string to prevent failure of featureDB object creation

--- src/annot_consistency/gffutils_db.py
+++ src/annot_consistency/gffutils_db.py
@@ -10,17 +10,17 @@ from gffutils.interface import FeatureDB
 def load_or_create_db(gff_file_a: Path, gff_file_b: Path, db_path_a: Path, db_path_b: Path) -> tuple[FeatureDB, FeatureDB]:
 
     try: 
-        db_a = gffutils.FeatureDB(db_path_a)
+        db_a = gffutils.FeatureDB(str(db_path_a))
 
     except:
-        db_a = gffutils.create_db(gff_file_a, dbfn = db_path_a, keep_order = True)        
+        db_a = gffutils.create_db(str(gff_file_a), dbfn = str(db_path_a), keep_order = True)        
 
             
     try:
-        db_b = gffutils.FeatureDB(db_path_b)
+        db_b = gffutils.FeatureDB(str(db_path_b))
             
     except:
-       db_b = gffutils.create_db(gff_file_b, dbfn = db_path_b, keep_order = True)
+       db_b = gffutils.create_db(str(gff_file_b), dbfn = str(db_path_b), keep_order = True)
 
 
     return db_a, db_b
