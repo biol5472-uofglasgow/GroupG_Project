e803f014ced06d416e8e482d4772fc080b708752
unknown <sahesh02@gmail.com>
2026-01-28 18:31:08 +0000

    Added value error except blocks to prevent bare excepts and formatted according to ruff linting

--- src/annot_consistency/gffutils_db.py
+++ src/annot_consistency/gffutils_db.py
@@ -1,25 +1,29 @@
-import gffutils
-from typing import tuple
 from pathlib import Path
-from gffutils.interface import FeatureDB
 
-#Function to load the pre-existing database or create one for gff files 
+import gffutils
+from gffutils import FeatureDB
+
+#Function to load the pre-existing database or create one for gff files
 # If DB exists, it connects to it
 # If DB does not exist, it creates a DB using the gff file
 
-def load_or_create_db(gff_file_a: Path, gff_file_b: Path, db_path_a: Path, db_path_b: Path) -> tuple[FeatureDB, FeatureDB]:
+def load_or_create_db(
+    gff_file_a: Path,
+    gff_file_b: Path,
+    db_path_a: Path,
+    db_path_b: Path) -> tuple[FeatureDB, FeatureDB]:
 
-    try: 
+    try:
         db_a = gffutils.FeatureDB(str(db_path_a))
 
-    except:
-        db_a = gffutils.create_db(str(gff_file_a), dbfn = str(db_path_a), keep_order = True)        
+    except ValueError:
+        db_a = gffutils.create_db(str(gff_file_a), dbfn = str(db_path_a), keep_order = True)
 
-            
+
     try:
         db_b = gffutils.FeatureDB(str(db_path_b))
-            
-    except:
+
+    except ValueError:
        db_b = gffutils.create_db(str(gff_file_b), dbfn = str(db_path_b), keep_order = True)
 
 
