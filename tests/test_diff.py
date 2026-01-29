3b7d24d9672796a3b94a1c56f1d7a1b8dd57df3d
unknown <sahesh02@gmail.com>
2026-01-29 00:55:43 +0000

    Created test for changed_details function in test_diff file

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,1 +1,41 @@
- 
+from src.annot_consistency.diff import changed_details
+from src.annot_consistency.models import EntitySummary
+from typing import Mapping, Literal
+
+
+def make_EntitySummary_instance(entity_type: str, entity_id: str, seqid:str, start:int,
+                        end:int, strand:str, parent_id:str, attrs:Mapping[str,str], score:float, phase:Literal[0,1,2],
+                        source:str):
+    
+    return EntitySummary(
+        entity_type = entity_type,
+        entity_id = entity_id,
+        seqid = seqid,
+        start = start,
+        end = end,
+        strand = strand,
+        parent_id = parent_id,
+        attrs = attrs,
+        score = score,
+        phase = phase,
+        source = source
+
+    )
+
+def test_changed_details():
+    a = make_EntitySummary_instance("mRNA", "tx1", "chr1", 5, 80, "+", "gene1", {'ID':'tx1', 'Parent':'gene1'}, 0.0, 0, "fixture")
+    b = make_EntitySummary_instance("mRNA", "tx2", "chr2", 1, 20, "-", "gene2", {'ID':'tx2', 'Parent':'gene2'}, 0.0, 0, "fixture")
+
+    change = changed_details(a,b)
+
+    assert "seqid: chr1 -> chr2" in change
+    assert "Start: 5 -> 1" in change
+    assert "End: 80 -> 20" in change
+    assert "Parent ID: gene1 -> gene2" in change    
+    assert "Strand: + -> -" in change
+    assert "Entity Type:" not in change
+    assert "Score:" not in change
+    assert "Phase:" not in change
+
+
+
