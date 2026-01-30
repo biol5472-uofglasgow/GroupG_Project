54a29f465120f9468ef01c84871fbf3d20d86770
KaiserWaffles <148008162+KaiserWaffles@users.noreply.github.com>
2026-01-30 14:21:23 +0000

    ran ruff check --fix

--- src/annot_consistency/logging_utils.py
+++ src/annot_consistency/logging_utils.py
@@ -1,9 +1,10 @@
 import logging
 
+
 def logger(log_file: str) -> logging.Logger:
     logger = logging.getLogger("gffACAKE")
     logger.setLevel(logging.DEBUG)      # Set level to Debug; capture everything
-    if logger.handlers:                                           
+    if logger.handlers:
         logger.handlers.clear()     # prevent duplicate logs if rerun
     format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")       # logger output message
     filehandler = logging.FileHandler(log_file)
