33c92e9b28aca4af4c99e71c917f25df860b9b8b
KaiserWaffles <148008162+KaiserWaffles@users.noreply.github.com>
2026-01-30 14:25:34 +0000

    ruff check: Remove training whitespace

--- src/annot_consistency/logging_utils.py
+++ src/annot_consistency/logging_utils.py
@@ -5,19 +5,19 @@ def logger(log_file: str) -> logging.Logger:
     logger = logging.getLogger("gffACAKE")
     # Set level to Debug; capture everything
 
-    logger.setLevel(logging.DEBUG)    
+    logger.setLevel(logging.DEBUG)
 
     # prevent duplicate logs if rerun
     if logger.handlers:
-        logger.handlers.clear()     
+        logger.handlers.clear()
 
     # logger output message
-    format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")       
+    format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")
     filehandler = logging.FileHandler(log_file)
     filehandler.setLevel(logging.DEBUG)
     filehandler.setFormatter(format)
     logger.addHandler(filehandler)
     # Prevent log records from propagating to parent loggers
-    logger.propagate = False        
+    logger.propagate = False  
     return logger
 
