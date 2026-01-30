8d1b41fea0e70b4f34f5990f7a9160eac0f865a6
KaiserWaffles <148008162+KaiserWaffles@users.noreply.github.com>
2026-01-30 14:23:46 +0000

    ruff check: Line too long (103 > 100)

--- src/annot_consistency/logging_utils.py
+++ src/annot_consistency/logging_utils.py
@@ -3,14 +3,21 @@ import logging
 
 def logger(log_file: str) -> logging.Logger:
     logger = logging.getLogger("gffACAKE")
-    logger.setLevel(logging.DEBUG)      # Set level to Debug; capture everything
+    # Set level to Debug; capture everything
+
+    logger.setLevel(logging.DEBUG)    
+
+    # prevent duplicate logs if rerun
     if logger.handlers:
-        logger.handlers.clear()     # prevent duplicate logs if rerun
-    format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")       # logger output message
+        logger.handlers.clear()     
+
+    # logger output message
+    format = logging.Formatter("%(levelname)s\t%(asctime)s\t%(message)s")       
     filehandler = logging.FileHandler(log_file)
     filehandler.setLevel(logging.DEBUG)
     filehandler.setFormatter(format)
     logger.addHandler(filehandler)
-    logger.propagate = False        # Prevent log records from propagating to parent loggers.
+    # Prevent log records from propagating to parent loggers
+    logger.propagate = False        
     return logger
 
