d9d361680576c5f784071ad5999fe47fc64bf7f2
kro7ks <kksameer98@gmail.com>
2026-02-03 18:58:49 +0000

    Updating io for dealing with mypy errors

--- src/annot_consistency/io.py
+++ src/annot_consistency/io.py
@@ -80,7 +80,7 @@ def write_tracks(path: str, entities: list[EntitySummary]) -> None:
             if e.parent_id:
                 attrs_parts.append(f'Parent={e.parent_id}')
             attrs = ';'.join(attrs_parts)
-            score = "." if e.score in (None, ".", "") else str(float(e.score))
+            score = "." if e.score in (None, ".", "") else str(e.score)
             phase = "." if e.phase in (None, ".", "") else str(e.phase)
             strand = "." if e.strand in (None, "", ".") else e.strand
             track.write(f'{e.seqid}\tgffACAKE\t{e.entity_type}\t{int(e.start)}\t{int(e.end)}\t{score}\t{strand}\t{phase}\t{attrs}\n')
